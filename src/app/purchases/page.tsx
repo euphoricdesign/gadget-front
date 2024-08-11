093d1681b6a32bd323f2dea5a8bec587a85bf2ba
Merlina <noahchukita@gmail.com>
2024-08-11 15:21:40 -0300

    feat: crea componente PurchasedProductCard y reemplazar map por flatMap para manejar datos anidados en /purchases

--- src/app/purchases/page.tsx
+++ src/app/purchases/page.tsx
@@ -1,99 +1,103 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import axios, { AxiosResponse } from 'axios'
-import ProductCard from '@/components/ProductCard/ProductCard';
-import styles from '../../components/CardHome/CardHome.module.css'
-import Link from 'next/link';
-import { getEnvVariables } from '@/helpers/getEnvVariables';
+"use client";
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+import ProductCard from "@/components/ProductCard/ProductCard";
+import styles from "../../components/CardHome/CardHome.module.css";
+import Link from "next/link";
+import { getEnvVariables } from "@/helpers/getEnvVariables";
+import PurchasedProductCard from "@/components/PurchasedProductCard/PurchasedProductCard";
+import { Purchased } from "@/types/types";
 
-interface Product {
-  id: string;
-  image: string;
-  name: string;
-  price: number;
-}
 
-interface Purchase {
-  products: Product[];
-}
 
-interface UserSession {
-  token: string;
-}
+
 
 const Purchases = () => {
   const [userSession, setUserSession] = useState<any>(null);
-  const [userPurchaseData, setUserPurchaseData] = useState<Purchase[]>([])
+  const [userPurchaseData, setUserPurchaseData] = useState<Purchased[]>([]);
+  console.log(userPurchaseData);
 
   const envVars = getEnvVariables();
 
   // Efecto para obtener datos de localStorage al montar el componente
   useEffect(() => {
     try {
-      if (typeof window !== 'undefined') {
+      if (typeof window !== "undefined") {
         const storedUserSession = localStorage.getItem("userSession");
         if (storedUserSession) {
           setUserSession(JSON.parse(storedUserSession));
         }
-      }   
+      }
     } catch (error) {
-      console.error('Error al leer del localStorage:', error);
+      console.error("Error al leer del localStorage:", error);
     }
   }, []);
 
   useEffect(() => {
     const getPurchaseData = async () => {
       if (!userSession?.token) return;
-  
+
       const config = {
         headers: {
-          'Authorization': userSession.token
-        }
+          Authorization: userSession.token,
+        },
       };
-  
+
       try {
-        const purchaseData: AxiosResponse = await axios.get(`${envVars.NEXT_PUBLIC_BACK_URL}/users/orders`, config);
+        const purchaseData: AxiosResponse = await axios.get(
+          `${envVars.NEXT_PUBLIC_BACK_URL}/users/orders`,
+          config,
+        );
         setUserPurchaseData(purchaseData.data);
       } catch (error) {
         console.error("Error fetching purchase data:", error);
         // Aquí podrías manejar el error, por ejemplo, mostrando un mensaje al usuario
       }
     };
-  
+
     getPurchaseData();
   }, [userSession, userSession?.token, envVars.NEXT_PUBLIC_BACK_URL]);
-  
+
   return (
     <div>
-        <h2 className='text-2xl font-bold text-[#454545] mb-4 mt-10'>My purchases</h2>
-        <div>
-            {
-                userPurchaseData.length > 0 ? (
-                    <div className='flex gap-8 my-12 flex-wrap desktop:justify-start mobile:justify-center'>
-                        {
-                            userPurchaseData.map((purchase, index) => (
-                                <div key={index}>
-                                    {
-                                        purchase.products.map(product => (
-                                            <ProductCard key={product.id} id={product.id} image={product.image} name={product.name} price={product.price} />
-                                        ))
-                                    }
-                                </div>
-                            ))
-                        }
-                    </div>
-                ) : (
-                  <div className='flex gap-8 my-12 justify-center'>
-                    <div className="flex flex-col items-center" style={{height: '23rem'}}>
-                      <p className="text-lg">You have not yet made a purchase 💔</p>
-                      <button className={`${styles.button} flex justify-center items-center gap-4 shadow-sm w-72 border-slate-800 bg-[#1A1A1A] text-white text-sm font-medium mt-4 hover:-translate-y-1 transition p-3 duration-300 rounded border-2`}><Link href="/">Ver productos</Link></button>
-                    </div>
-                  </div>
-                )
-            }
-        </div>
+      <h2 className="text-2xl font-bold text-[#454545] mb-4 mt-10">
+        My purchases
+      </h2>
+      <div>
+        {userPurchaseData.length > 0 ? (
+          <div className="flex gap-8 my-12 flex-wrap desktop:justify-between mobile:justify-center">
+            {userPurchaseData.flatMap((purchase, purchaseIndex) =>
+              purchase.products.map((product) => (
+                <PurchasedProductCard
+                  key={`${purchaseIndex}-${product.id}`}
+                  status={purchase.status}
+                  date={purchase.date}
+                  id={product.id}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                />
+              )),
+            )}
+          </div>
+        ) : (
+          <div className="flex gap-8 my-12 justify-center">
+            <div
+              className="flex flex-col items-center"
+              style={{ height: "23rem" }}
+            >
+              <p className="text-lg">You have not yet made a purchase 💔</p>
+              <button
+                className={`${styles.button} flex justify-center items-center gap-4 shadow-sm w-72 border-slate-800 bg-[#1A1A1A] text-white text-sm font-medium mt-4 hover:-translate-y-1 transition p-3 duration-300 rounded border-2`}
+              >
+                <Link href="/">Ver productos</Link>
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Purchases
+export default Purchases;
---
+++ src/components/PurchasedProductCard/PurchasedProductCard.tsx
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import React from "react";
+
+interface PurchasedProductCardProps {
+  id: string;
+  status: string;
+  date: string;
+  image: string;
+  name: string;
+  price: number;
+}
+
+const PurchasedProductCard: React.FC<PurchasedProductCardProps> = ({
+  id,
+  status,
+  date,
+  image,
+  name,
+  price,
+}) => {
+  return (
+    <div className="bg-white shadow-md rounded-md overflow-hidden p-4 h-22rem w-[14rem]">
+      <Link href={`/product/${id}`} className="flex justify-center">
+        <img src={image} alt={name} className="w-[150px] object-contain" />
+      </Link>
+
+      <div className="p-4 flex flex-col  mobile:gap-1">
+        <h3 className="desktop:text-[15px] font-semibold mb-2 mobile:text-sm">
+          {name}
+        </h3>
+        <div className="flex justify-between flex-col gap-[6px]">
+          <div className="flex items-center gap-1">
+            <p className="text-gray-400 text-sm">Price:</p>
+            <p className="text-[12px]">
+              ${price}
+            </p>
+          </div>
+          <div className="flex items-center gap-1">
+            <p className="text-gray-400 text-sm">Status:</p>
+            <p className="text-[12px] text-green-500">
+              {status}
+            </p>
+          </div>
+          <div className="flex items-center gap-1">
+            <p className="text-gray-400 text-sm">Date:</p>
+            <p className="text-[12px]">
+              {" "}
+              {new Date(date)
+                .toLocaleDateString("es-ES", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                })
+                .replace(/\//g, "/")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PurchasedProductCard;
