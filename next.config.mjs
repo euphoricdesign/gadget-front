0e8e7270fbde015b1fda58bea82e48a2fb894a71
Merlina <noahchukita@gmail.com>
2024-08-12 01:20:35 -0300

    config: agregar dominios de imágenes a en next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['d28hi93gr697ol.cloudfront.net', 'encrypted-tbn2.gstatic.com','ofertones.com.ar', 'mac-center.com.pe', 'plazavea.vteximg.com.br', 'http2.mlstatic.com', 'www.megatone.net'],
+        domains: ['d28hi93gr697ol.cloudfront.net', 'encrypted-tbn2.gstatic.com','ofertones.com.ar', 'mac-center.com.pe', 'plazavea.vteximg.com.br', 'http2.mlstatic.com', 'www.megatone.net', 'ar.oneclickstore.com', 'm.media-amazon.com', 'store.storeimages.cdn-apple.com', 'dcdn.mitiendanube.com', 'cdn-ipoint.waugi.com.ar', 'thegoodguys.sirv.com', 'i.ebayimg.com', 'imagedelivery.net', 'rossellimac.es'],
     }
 };
 
